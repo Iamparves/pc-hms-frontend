5bc3ae42fab57f69e77b5b24d252a637554ad6fe
iamparves <parves.pe.wb.h@gmail.com>
2024-05-28 17:14:27 +0600

    setup shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-import React from "react";
-
 const App = () => {
-  return (
-    <div className="p-5 m-5 font-bold md:text-4xl text-3xl py-3 ">App</div>
-  );
+  return <div className="m-5 p-5 py-3 text-3xl font-bold md:text-4xl"></div>;
 };
 
 export default App;
