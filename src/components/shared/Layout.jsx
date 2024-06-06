7191b0a568ec6a762b707c45b1fca814377f80c6
iamparves <parves.pe.wb.h@gmail.com>
2024-06-06 22:33:32 +0600

    footer added

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 const Layout = () => {
   return (
     <div className="min-h-[100dvh]">
       <Navbar />
-      <Outlet />
+      <div className="min-h-[calc(100vh-140px)]">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 };
