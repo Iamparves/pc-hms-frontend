82088ab83730c33ff217548fa10774ba61ccd43f
iamparves <parves.pe.wb.h@gmail.com>
2024-06-10 18:07:21 +0600

    about page added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,19 @@
+import AboutUs from "@/components/Home/AboutUs";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <main>
+      <div className="bg-[url(/about.jpg)] bg-cover bg-center">
+        <div className="bg-black/50 py-20">
+          <h1 className="text-center text-4xl font-bold text-white md:text-5xl">
+            About Us
+          </h1>
+        </div>
+      </div>
+      <AboutUs />
+      <div className="border-b border-gray-200/50"></div>
+    </main>
+  );
 };
 
 export default About;
